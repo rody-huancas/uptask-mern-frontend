3869c635e615901d0b99611c3d06177c7420f675
Rody Huancas <rodyhuancas.04@gmail.com>
2024-07-14 12:19:56 -0500

    Feat: list projects

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,11 +1,29 @@
 import api from "@/lib/axios";
-import { ProjectFormData } from "../types";
+
+import { isAxiosError } from "axios";
+import { dashboardProjectSchema, ProjectFormData } from "../types";
 
 export async function createProject(formData: ProjectFormData) {
   try {
     const { data } = await api.post("/projects", formData);
     return data;
   } catch (error) {
-    console.log(error);
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function getProjects() {
+  try {
+    const { data } = await api("/projects");
+    const response = dashboardProjectSchema.safeParse(data);
+    if (response.success) {
+      return response.data;
+    }
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
   }
 }
