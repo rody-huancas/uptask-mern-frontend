c8d757721c6bd834aac9a807b4717c65f4b3a1e4
Rody Huancas <rodyhuancas.04@gmail.com>
2024-07-13 20:43:11 -0500

    Feat: configuration for tanstack react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import Router from "./router";
 
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router />
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
   </React.StrictMode>
 );
