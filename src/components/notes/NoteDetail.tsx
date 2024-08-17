2dd4bf0a8e934dbc98b289613387b6dc49263253
Rody Huancas <rodyhuancas.04@gmail.com>
2024-08-16 22:40:13 -0500

    Feat: delete note

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,13 +1,44 @@
+import { useMemo } from "react";
+
+import { toast } from "react-toastify";
+import { useLocation, useParams } from "react-router-dom";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
 import { Note } from "@/types/index";
+import { useAuth } from "@/hooks/useAuth";
 import { formatDate } from "@/utils/utils";
+import { deleteNote } from "@/api/NoteAPI";
 
 type NoteDetailProps = {
   note: Note;
 };
 
 const NoteDetail = ({ note }: NoteDetailProps) => {
+  const { data, isLoading } = useAuth();
+  const canDelete = useMemo(() => data?._id === note.createdBy._id, [data]);
+
+  const params = useParams();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+
+  const projectId = params.projectId!;
+  const taskId = queryParams.get("viewTask")!;
+
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: deleteNote,
+    onError: (error) => toast.error(error.message),
+    onSuccess: (data) => {
+      toast.success(data);
+      queryClient.invalidateQueries({ queryKey: ["task", taskId] });
+    },
+  });
+
+  if (isLoading) return "Cargando...";
+
   return (
-    <div className="p-3 flez justify-center items-center">
+    <div className="p-3 flex justify-between items-center">
       <div>
         <p>
           {note.content} por:{" "}
@@ -15,6 +46,16 @@ const NoteDetail = ({ note }: NoteDetailProps) => {
         </p>
         <p className="text-xs text-slate-500">{formatDate(note.createdAt)}</p>
       </div>
+
+      {canDelete && (
+        <button
+          type="button"
+          className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors"
+          onClick={() => mutate({ projectId, taskId, noteId: note._id })}
+        >
+          Eliminar
+        </button>
+      )}
     </div>
   );
 };
