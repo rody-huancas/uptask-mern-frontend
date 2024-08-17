1e62f25575ba0eb74bbd5bd99dc7d7a4a67a836b
Rody Huancas <rodyhuancas.04@gmail.com>
2024-08-16 22:15:12 -0500

    Feat: create note

--- src/components/notes/AddNoteForm.tsx
+++ src/components/notes/AddNoteForm.tsx
@@ -1,6 +1,43 @@
+import { toast } from "react-toastify";
+import { useForm } from "react-hook-form";
+import { useLocation, useParams } from "react-router-dom";
+
+import ErrorMessage from "../ErrorMessage";
+
+import { createNote } from "@/api/NoteAPI";
+import { NoteFormData } from "@/types/index";
+import { useMutation } from "@tanstack/react-query";
+
 const AddNoteForm = () => {
+  const params = useParams();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+
+  const projectId = params.projectId!;
+  const taskId = queryParams.get('viewTask')!;
+
+  const initialValues: NoteFormData = { content: '' }
+
+  const { register, handleSubmit, formState: { errors }, reset } = useForm({ defaultValues: initialValues });
+
+  const { mutate } = useMutation({
+    mutationFn: createNote,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      toast.success(data);
+    },
+  })
+
+  const handleAddNote = (formData: NoteFormData) => {
+    mutate({ projectId, taskId, formData });
+    reset();
+  }
+
   return (
-    <form onSubmit={() => {}} className="space-y-3" noValidate>
+    <form onSubmit={handleSubmit(handleAddNote)} className="space-y-3" noValidate>
       <div className="flex flex-col gap-2">
         <label htmlFor="content" className="font-bold">
           Crear Nota
@@ -10,7 +47,13 @@ const AddNoteForm = () => {
           id="content"
           placeholder="Contenido de la nota"
           className="w-full p-3 border border-gray-300"
+          {...register('content', {
+            required: 'El contenido de la nota es obligatorio'
+          })}
         />
+        { errors.content && (
+          <ErrorMessage>{errors.content.message}</ErrorMessage>
+        ) }
       </div>
 
       <input
--- src/types/index.ts
+++ src/types/index.ts
@@ -24,7 +24,20 @@ export const userSchema = authSchema.pick({
 }).extend({
   _id: z.string()
 });
+
 export type User = z.infer<typeof userSchema>
+
+// Notes
+
+export const noteSchema = z.object({
+  _id      : z.string(),
+  content  : z.string(),
+  createdBy: userSchema,
+  task     : z.string()
+});
+
+export type Note = z.infer<typeof noteSchema>;
+export type NoteFormData = Pick<Note, 'content'>;
 
 // Tasks
 export const taskStatusSchema = z.enum(['pending', 'onHold', 'inProgress', 'underReview', 'completed']);
