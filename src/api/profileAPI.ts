1dfdf21e8399dbbc65f11184543224b89000be7f
Rody Huancas <rodyhuancas.04@gmail.com>
2024-08-17 19:26:04 -0500

    Feat: update password

--- src/api/profileAPI.ts
+++ src/api/profileAPI.ts
@@ -1,11 +1,23 @@
 import { isAxiosError } from "axios";
 
 import api from "@/lib/axios";
-import { UserProfileForm } from "../types";
+import { UpdateCurrentPasswordForm, UserProfileForm } from "../types";
 
 export async function updateProfile(formData: UserProfileForm) {
   try {
     const { data } = await api.put<string>("/auth/profile", formData);
+
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function changePassword(formData: UpdateCurrentPasswordForm) {
+  try {
+    const { data } = await api.post<string>("/auth/update-password", formData);
 
     return data;
   } catch (error) {
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,20 +2,21 @@ import { z } from "zod";
 
 // Auth & Users
 export const authSchema = z.object({
-  name    : z.string(),
-  email   : z.string().email(),
-  password: z.string(),
+  name                 : z.string(),
+  email                : z.string().email(),
+  current_password     : z.string(),
+  password             : z.string(),
   password_confirmation: z.string(),
-  token   : z.string(),
+  token                : z.string(),
 });
 type Auth = z.infer<typeof authSchema>
-export type UserLoginForm = Pick<Auth, 'email' | 'password'>
-export type UserRegistrationForm = Pick<Auth, 'name' | 'email' | 'password' | 'password_confirmation'>
+export type UserLoginForm               = Pick<Auth, 'email' | 'password'>
+export type UserRegistrationForm        = Pick<Auth, 'name' | 'email' | 'password' | 'password_confirmation'>
 export type RequestConfirmationCodeForm = Pick<Auth, 'email'>
-export type ForgotPasswordForm = Pick<Auth, 'email'>
-export type NewPasswordForm = Pick<Auth, 'password' | 'password_confirmation'>
-
-export type ConfirmToken = Pick<Auth, 'token'>
+export type ForgotPasswordForm          = Pick<Auth, 'email'>
+export type NewPasswordForm             = Pick<Auth, 'password' | 'password_confirmation'>
+export type UpdateCurrentPasswordForm   = Pick<Auth, 'current_password' | 'password' | 'password_confirmation'>
+export type ConfirmToken                = Pick<Auth, 'token'>
 
 // Users
 export const userSchema = authSchema.pick({
